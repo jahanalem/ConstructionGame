f35d09e3712eb583b09dd2fcb8c07aa4128674ea
jahanalem <s.r.alem19@gmail.com>
2024-10-20 21:15:05 +0200

    Add project files.

---
+++ ConstructionGame.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionGame
+{
+    public class ConstructionGame
+    {
+        private int[,] _table;
+        private int _length;
+        private int _width;
+
+        public ConstructionGame(int length, int width)
+        {
+            this._length = length;
+            this._width = width;
+            this._table = new int[length, width];
+        }
+
+        public void AddCubes(bool[,] cubes)
+        {
+            for (int i = 0; i < _length; i++)
+            {
+                for (int j = 0; j < _width; j++)
+                {
+                    if (cubes[i, j])
+                    {
+                        _table[i, j]++; // add height
+                    }
+                }
+            }
+        }
+
+        public int GetHeight()
+        {
+            var isFullLevel = true; // Check if the entire table area is filled with cubes or not.
+            while (isFullLevel)
+            {
+                for (int i = 0; i < _length; i++)
+                {
+                    for (int j = 0; j < _width; j++)
+                    {
+                        if (_table[i, j] == 0)
+                        {
+                            isFullLevel = false;
+                            break;
+                        }
+                    }
+                }
+                if (isFullLevel)
+                {
+                    for (int i = 0; i < _length; i++)
+                    {
+                        for (int j = 0; j < _width; j++)
+                        {
+                            _table[i, j] -= 1; // if the whole cell of the table is fulled with cubes then we reduce the hieght.
+                        }
+                    }
+                }
+            }
+
+            int maxHeight = 0;
+            for (int i = 0; i < _length; i++)
+            {
+                for (int j = 0; j < _width; j++)
+                {
+                    if (_table[i, j] > maxHeight)
+                    {
+                        maxHeight = _table[i, j];
+                    }
+                }
+            }
+
+            return maxHeight;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ConstructionGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConstructionGame game = new ConstructionGame(2, 2);
+            game.AddCubes(new bool[,]
+            {
+                { true, true },
+                { true, true }
+            });
+            game.AddCubes(new bool[,]
+            {
+                { true, true },
+                { true, true }
+            });
+            game.AddCubes(new bool[,]
+            {
+                { true, true },
+                { false, false }
+            });
+            game.AddCubes(new bool[,]
+            {
+                { true, true },
+                { false, true }
+            });
+            Console.WriteLine(game.GetHeight()); // Should print 2
+
+            game.AddCubes(new bool[,]
+            {
+                { false, false },
+                { true, true }
+            });
+            Console.WriteLine(game.GetHeight()); // Should print 1
+
+            game.AddCubes(new bool[,]
+            {
+                { false, false },
+                { true, false }
+            });
+            Console.WriteLine(game.GetHeight()); // Should print 0
+        }
+    }
+}
